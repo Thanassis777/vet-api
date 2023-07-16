43ec01908e42dc02b221635cf65ddaf4a51548d8
Thanassis777 <thanassis77@hotmail.gr>
2023-07-16 20:01:03 +0300

    minor refactor2

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,8 @@ import * as path from "path";
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
+app.use('/img',express.static(path.join(__dirname, 'public/img')));
 
 // Set security HTTP headers
 app.use(helmet());
